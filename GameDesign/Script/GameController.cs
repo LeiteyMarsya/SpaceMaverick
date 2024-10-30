484fb87b4dc3e089044bac9f62fb98e40ff1890c
LeiteyMarsya <leitey123@gmail.com>
2024-10-31 02:50:47 +0800

    Update GameController.cs

--- GameDesign/Script/GameController.cs
+++ GameDesign/Script/GameController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class GameController : MonoBehaviour
 {
@@ -30,9 +32,41 @@ public class GameController : MonoBehaviour
     [field: SerializeField]
     public List<EnemyShipController> enemies { get; private set; }
 
-    // Start is called before the first frame update
+    [field: SerializeField]
+    public static GameController Instance;
+
+    [field: SerializeField]
+    private int _score;
+
+    public int Score
+    {
+        get => _score;
+        private set
+        {
+            _score = value;
+            OnScoreChange.Invoke(_score);
+        }
+    }
+
+    public UnityEngine.Events.UnityEvent<int> OnScoreChange;
+
+    public UnityEngine.Events.UnityEvent<int> OnLivesChange;
+
+    [field: SerializeField]
+    private int _livesRemaining = 3;
+    public int LivesRemaining
+    {
+        get => _livesRemaining;
+        private set
+        {
+            _livesRemaining = value;
+            OnLivesChange.Invoke(_livesRemaining);
+        }
+    }
+
     void Start()
     {
+        LivesRemaining = 3;
         SpawnPlayer();
         List<Transform> waypoints = new() { WayPoints[0], WayPoints[1], WayPoints[2] };
         IEnemyShip enemy = new EnemyShip(EnemyTypes[0], waypoints, EnemySpawnPoint.position);
@@ -55,15 +89,45 @@ public class GameController : MonoBehaviour
 
     private void SpawnPlayer()
     {
-        PlayerController pc = Instantiate(PlayerTemplate);
-        pc.GameController = this;
-        pc.transform.position = PlayerSpawnPoint.position;
-        SpawnAt = -1;
+        if (LivesRemaining >0)
+        {
+            PlayerController pc = Instantiate(PlayerTemplate);
+            pc.GameController = this;
+            pc.transform.position = PlayerSpawnPoint.position;
+            SpawnAt = -1;
+        }
     }
 
     public void DestroyPlayer(PlayerController toDestroy)
     { 
         Destroy(toDestroy.gameObject);
         SpawnAt = Time.time + SpawnDelay;
+        LivesRemaining--;
     }
+
+    private void Awake()
+    {
+        // Set up singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        Score += points;
+    }
+
+    public int GetScore()
+    {
+        return Score;
+    }
+
+
 }
