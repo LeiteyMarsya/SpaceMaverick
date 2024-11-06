0388832fa0395a7c6990d71d8ef09682d8f3a4f3
LeiteyMarsya <leitey123@gmail.com>
2024-11-07 01:48:32 +0800

    Update GameController.cs

--- GameDesign/Script/GameController.cs
+++ GameDesign/Script/GameController.cs
@@ -2,42 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
 
 public class GameController : MonoBehaviour
 {
-    [field: SerializeField]
+    [field: SerializeField] 
     public List<EnemyShipController> EnemyTypes { get; private set; }
 
-    [field:SerializeField]
-    public Transform EnemySpawnPoint {  get; private set; }
+    [field: SerializeField] 
+    public Transform EnemySpawnPoint { get; private set; }
 
-    [field:SerializeField]
+    [field: SerializeField] 
     public List<Transform> WayPoints { get; set; }
 
-    [field: SerializeField]
-    public EnemySpawner  EnemySpawner {  get; private set; }
+    [field: SerializeField] 
+    public EnemySpawner EnemySpawner { get; private set; }
 
-    [field: SerializeField]
+    [field: SerializeField] 
     public PlayerController PlayerTemplate { get; private set; }
 
-    [field: SerializeField]
+    [field: SerializeField] 
     public Transform PlayerSpawnPoint { get; private set; }
 
-    [field: SerializeField]
+    [field: SerializeField] 
     public float SpawnDelay { get; private set; } = 3;
 
-    [field: SerializeField]
+    [field: SerializeField] 
     public float SpawnAt { get; private set; } = -1;
-    [field: SerializeField]
+
+    [field: SerializeField] 
     public List<EnemyShipController> enemies { get; private set; }
 
-    [field: SerializeField]
+    [field: SerializeField] 
     public static GameController Instance;
 
-    [field: SerializeField]
-    private int _score;
-
+    [field: SerializeField] private int _score;
     public int Score
     {
         get => _score;
@@ -45,15 +43,14 @@ public class GameController : MonoBehaviour
         {
             _score = value;
             OnScoreChange.Invoke(_score);
+            CheckHighScore();  // Check high score when score changes
         }
     }
 
     public UnityEngine.Events.UnityEvent<int> OnScoreChange;
-
     public UnityEngine.Events.UnityEvent<int> OnLivesChange;
 
-    [field: SerializeField]
-    private int _livesRemaining = 3;
+    [field: SerializeField] private int _livesRemaining = 3;
     public int LivesRemaining
     {
         get => _livesRemaining;
@@ -64,10 +61,24 @@ public class GameController : MonoBehaviour
         }
     }
 
+    [field: SerializeField] 
+    public GameObject GameOverScreen { get; private set; }
+
+    [field: SerializeField] 
+    private int highScore;
+
+    [field: SerializeField] 
+    private TextMeshProUGUI highScoreText;
+
     void Start()
     {
         LivesRemaining = 3;
         SpawnPlayer();
+
+        // Load the high score at the start of the game
+        LoadHighScore();
+        UpdateHighScoreText();
+
         List<Transform> waypoints = new() { WayPoints[0], WayPoints[1], WayPoints[2] };
         IEnemyShip enemy = new EnemyShip(EnemyTypes[0], waypoints, EnemySpawnPoint.position);
 
@@ -78,7 +89,6 @@ public class GameController : MonoBehaviour
         EnemySpawner.EnqueueEnemies(enemies);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (SpawnAt > 0 && Time.time > SpawnAt)
@@ -89,7 +99,7 @@ public class GameController : MonoBehaviour
 
     private void SpawnPlayer()
     {
-        if (LivesRemaining >0)
+        if (LivesRemaining > 0)
         {
             PlayerController pc = Instantiate(PlayerTemplate);
             pc.GameController = this;
@@ -99,10 +109,45 @@ public class GameController : MonoBehaviour
     }
 
     public void DestroyPlayer(PlayerController toDestroy)
-    { 
+    {
         Destroy(toDestroy.gameObject);
         SpawnAt = Time.time + SpawnDelay;
         LivesRemaining--;
+        if (LivesRemaining <= 0)
+        {
+            GameOverScreen.SetActive(true);
+        }
+    }
+
+    public void Restart()
+    {
+        // Hide the game over screen
+        GameOverScreen.SetActive(false);
+
+        // Reset the score
+        Score = 0;
+
+        // Reset lives
+        LivesRemaining = 3;
+
+        // Clear any remaining enemies, lasers, and asteroids
+        foreach (var enemy in FindObjectsOfType<EnemyShipController>())
+        {
+            Destroy(enemy.gameObject);
+        }
+
+        foreach (var laser in FindObjectsOfType<LaserController>())
+        {
+            Destroy(laser.gameObject);
+        }
+
+        foreach (var asteroid in FindObjectsOfType<AsteroidController>())
+        {
+            Destroy(asteroid.gameObject);
+        }
+
+        // Respawn the player
+        SpawnPlayer();
     }
 
     private void Awake()
@@ -129,5 +174,32 @@ public class GameController : MonoBehaviour
         return Score;
     }
 
+    private void LoadHighScore()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+    }
 
+    private void SaveHighScore()
+    {
+        PlayerPrefs.SetInt("HighScore", highScore);
+        PlayerPrefs.Save();
+    }
+
+    private void CheckHighScore()
+    {
+        if (Score > highScore)
+        {
+            highScore = Score;
+            SaveHighScore();
+            UpdateHighScoreText();
+        }
+    }
+
+    private void UpdateHighScoreText()
+    {
+        if (highScoreText != null)
+        {
+            highScoreText.text = "High Score: " + highScore;
+        }
+    }
 }
