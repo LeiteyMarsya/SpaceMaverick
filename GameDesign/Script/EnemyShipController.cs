a752423e50b4d70472b035040227159888940517
LeiteyMarsya <leitey123@gmail.com>
2024-11-07 21:13:12 +0800

    Update EnemyShipController.cs

--- GameDesign/Script/EnemyShipController.cs
+++ GameDesign/Script/EnemyShipController.cs
@@ -1,80 +1,187 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-[RequireComponent(typeof(DestructableController))]
-public class EnemyShipController : MonoBehaviour
+public class GameController : MonoBehaviour
 {
     [field: SerializeField]
-    public Transform FireSpawnPoint { get; private set; }
+    public List<EnemyShipController> EnemyTypes { get; private set; }
 
     [field: SerializeField]
-    public float Speed { get; private set; } = 3;
+    public Transform EnemySpawnPoint { get; private set; }
 
     [field: SerializeField]
-    public int direction = 1;
+    public EnemySpawner EnemySpawner { get; private set; }
 
     [field: SerializeField]
-    public GameObject LaserTemplate { get; private set; }
+    public PlayerController PlayerTemplate { get; private set; }
 
     [field: SerializeField]
-    public float Firerate { get; private set; } = 2;
+    public Transform PlayerSpawnPoint { get; private set; }
 
     [field: SerializeField]
-    private bool isMovingHorizontal = true;
+    public float SpawnDelay { get; private set; } = 3;
 
-    private float LastFire;
+    [field: SerializeField]
+    public float SpawnAt { get; private set; } = -1;
+
+    [field: SerializeField]
+    public List<EnemyShipController> enemies { get; private set; }
+
+    [field: SerializeField]
+    public static GameController Instance;
+
+    [field: SerializeField] private int _score;
+    public int Score
+    {
+        get => _score;
+        private set
+        {
+            _score = value;
+            OnScoreChange.Invoke(_score);
+            CheckHighScore();  // Check high score when score changes
+        }
+    }
+
+    public UnityEngine.Events.UnityEvent<int> OnScoreChange;
+    public UnityEngine.Events.UnityEvent<int> OnLivesChange;
+
+    [field: SerializeField] private int _livesRemaining = 3;
+    public int LivesRemaining
+    {
+        get => _livesRemaining;
+        private set
+        {
+            _livesRemaining = value;
+            OnLivesChange.Invoke(_livesRemaining);
+        }
+    }
+
+    [field: SerializeField]
+    public GameObject GameOverScreen { get; private set; }
+
+    [field: SerializeField]
+    private int highScore;
+
+    [field: SerializeField]
+    private TextMeshProUGUI highScoreText;
 
     void Start()
     {
-        LastFire = Time.time + 1f;
-    }
+        LivesRemaining = 3;
+        SpawnPlayer();
 
-    public static EnemyShipController Spawn(EnemyShipController enemy, GameController gameController)
-    {
-        EnemyShipController newEnemy = Instantiate(enemy);
-        newEnemy.GetComponent<DestructableController>().GameController = gameController;
-        newEnemy.transform.position = enemy.transform.position;
-        return newEnemy;
+        // Load the high score at the start of the game
+        LoadHighScore();
+        UpdateHighScoreText();
+
+        EnemySpawner.EnqueueEnemies(enemies);
     }
 
     void Update()
     {
-        MoveShip();
-        HandleFire();
-    }
-
-    private void HandleFire()
-    {
-        if (Time.time > (LastFire + Firerate))
+        if (SpawnAt > 0 && Time.time > SpawnAt)
         {
-            GameObject laser = Instantiate(LaserTemplate);
-            laser.transform.position = FireSpawnPoint.position;
-            LastFire = Time.time;
+            SpawnPlayer();
         }
     }
 
-    private void MoveShip()
+    private void SpawnPlayer()
     {
-        float newX = transform.position.x + (Speed * Time.deltaTime * direction);
-        float newY = transform.position.y;
-
-        if (newX > 3 || newX < -3)
+        if (LivesRemaining > 0)
         {
-            direction *= -1;
-            isMovingHorizontal = false;
+            PlayerController pc = Instantiate(PlayerTemplate);
+            pc.GameController = this;
+            pc.transform.position = PlayerSpawnPoint.position;
+            SpawnAt = -1;
+        }
+    }
+
+    public void DestroyPlayer(PlayerController toDestroy)
+    {
+        Destroy(toDestroy.gameObject);
+        SpawnAt = Time.time + SpawnDelay;
+        LivesRemaining--;
+        if (LivesRemaining <= 0)
+        {
+            GameOverScreen.SetActive(true);
+        }
+    }
+
+    public void Restart()
+    {
+        GameOverScreen.SetActive(false);
+        Score = 0;
+        LivesRemaining = 3;
+
+        foreach (var enemy in FindObjectsOfType<EnemyShipController>())
+        {
+            Destroy(enemy.gameObject);
         }
 
-        if (!isMovingHorizontal)
+        foreach (var laser in FindObjectsOfType<LaserController>())
         {
-            newY = transform.position.y + (Speed * Time.deltaTime * direction);
-
-            if (newY > 4.5f || newY < -4.5f)
-            {
-                direction *= -1;
-                isMovingHorizontal = true;
-            }
+            Destroy(laser.gameObject);
         }
 
-        transform.position = new Vector2(newX, newY);
+        foreach (var asteroid in FindObjectsOfType<AsteroidController>())
+        {
+            Destroy(asteroid.gameObject);
+        }
+
+        SpawnPlayer();
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        Score += points;
+    }
+
+    public int GetScore()
+    {
+        return Score;
+    }
+
+    private void LoadHighScore()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    private void SaveHighScore()
+    {
+        PlayerPrefs.SetInt("HighScore", highScore);
+        PlayerPrefs.Save();
+    }
+
+    private void CheckHighScore()
+    {
+        if (Score > highScore)
+        {
+            highScore = Score;
+            SaveHighScore();
+            UpdateHighScoreText();
+        }
+    }
+
+    private void UpdateHighScoreText()
+    {
+        if (highScoreText != null)
+        {
+            highScoreText.text = "High Score: " + highScore;
+        }
     }
 }
